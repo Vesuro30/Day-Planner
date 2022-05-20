5088a11268c8ab194e19b46eb8bf991dc90d2eb5
ledford30 <101572791+Vesuro30@users.noreply.github.com>
2022-05-20 15:19:16 -0600

    finished logic

--- script/script.js
+++ script/script.js
@@ -9,6 +9,12 @@
 
 //  Initialize variables
 var $currentDayP = $("p#currentDay");
+var sectionDivTaskEl = $("section div.task")
+var userInputTasks = $("section div.task textarea");
+var saveButton  = $("section div.save");
+var allTasks = [];
+//  Set global variables for the colors used to indicate whether a 
+//  task is in the past, present or future
 var colorPast = "#CCC";
 var colorFuture = "#6cea3a";
 var colorPresent = "#ea2525";
@@ -16,7 +22,7 @@ var colorPresent = "#ea2525";
 
 
 
-
+//  Get and display the current date and time
 $currentDayP.html(moment().format("MMM Do, YYYY h:mm a"));
 setInterval(function()
   {
@@ -30,38 +36,59 @@ setInterval(function()
 colorTimeslots();
 
 
-
+populateTasks();
 
 
 
 
   function colorTimeslots()
   {
-    //  color present div
+    //  color present div red
     var currentHour = moment().format("H");
     $("section#h" + currentHour + " div.task").css("background-color", colorPresent);
-    //  color future divs
+    //  color future divs green
     for (let i = currentHour * 1 + 1; i < 18; i++) 
     {
       $("section#h" + i + " div.task").css("background-color", colorFuture);
       
     }
-
-
-
-    console.log(currentHour);
   }
 
 
 
-
+  //  This function will read the local storage and get the values from a string and convert it
+  //  to an array to populate the saved task fields.
   function populateTasks()
   {
+    var allTasks = JSON.parse(localStorage.getItem("storedUserTasks"));
+    console.log(allTasks);
 
+    for (let i = 0; i < 9; i++) 
+    {
+      $("section#h" + Number(i + 9) + " textarea").val(allTasks[i]);
+      
+    }
   }
 
 
+  //  Add click handler to the save "buttons"
+  saveButton.click(function()
+  {
+    saveTasks();
+  });
+
+
+  //  This function takes the task fields as an array and converts the information to 
+  //  a string to store in local storage
   function saveTasks()
   {
+    allTasks = [];
+    for (let i = 0; i < 9; i++) 
+    {
+      allTasks[i] = $("section#h" + Number(i + 9) + " textarea").val();
+ 
+    }
+
+    localStorage.setItem("storedUserTasks", JSON.stringify(allTasks));
 
   }
